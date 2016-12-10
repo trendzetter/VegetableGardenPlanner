b7fbbc7a09fb718ede2802a8e579d3f0be65e8a0
Joris Willems <joris.willems@gmail.com>
2016-12-10 17:53:02 +0100

    pagination, search for varieties

--- modules/plant-varieties/client/controllers/list.plant-varieties.client.controller.js
+++ modules/plant-varieties/client/controllers/list.plant-varieties.client.controller.js
@@ -5,11 +5,39 @@
     .module('plant-varieties')
     .controller('PlantVarietiesListController', PlantVarietiesListController);
 
-  PlantVarietiesListController.$inject = ['PlantVarietiesService'];
+  PlantVarietiesListController.$inject = ['$filter','PlantVarietiesService'];
 
-  function PlantVarietiesListController(PlantVarietiesService) {
+  function PlantVarietiesListController($filter,PlantVarietiesService) {
     var vm = this;
+    vm.buildPager = buildPager;
+    vm.figureOutItemsToDisplay = figureOutItemsToDisplay;
+    vm.pageChanged = pageChanged;
 
-    vm.plantvarieties = PlantVarietiesService.query();
+    PlantVarietiesService.query(function (data) {
+      vm.plantvarieties = data;
+      vm.buildPager();
+    });
+
+    function buildPager() {
+      vm.pagedItems = [];
+      vm.itemsPerPage = 15;
+      vm.currentPage = 1;
+      vm.figureOutItemsToDisplay();
+    }
+
+    function figureOutItemsToDisplay() {
+      vm.filteredItems = $filter('filter')(vm.plantvarieties, {
+        $: vm.search
+      });
+      vm.filterLength = vm.filteredItems.length;
+      var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+      var end = begin + vm.itemsPerPage;
+      vm.pagedItems = vm.filteredItems.slice(begin, end);
+    }
+
+    function pageChanged() {
+      vm.figureOutItemsToDisplay();
+    }     
+
   }
 }());
